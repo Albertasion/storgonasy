2096106753a3252e597f99fad4ca2c11b1bfa853
Albertasion <93448198+Albertasion@users.noreply.github.com>
2022-02-20 00:49:18 +0200

    load_product

--- main.py
+++ main.py
@@ -3,14 +3,15 @@ import asyncio
 import aiohttp
 from fake_useragent import UserAgent
 import lxml
-from random import choice
-proxy_list = open("http_proxies.txt").readlines()
+# from random import choice
+# proxy_list = open("http_proxies.txt").readlines()
 class Parser:
     def __init__(self):
         self.basic_url = "https://storgom.ua"
         self.main_catalog_links = []
         self.sub_catalog_links = []
         self.all_links_category = []
+        self.all_products_links = []
         self.ua = UserAgent()
         self.headers = {
             "accept": "*/*",
@@ -38,40 +39,27 @@ class Parser:
                         href = sublink['href']
                         url = f'{self.basic_url}{href}'
                         self.sub_catalog_links.append(url)
-
-    async def get_pagination(self):
-        async with aiohttp.ClientSession() as session:
-            for link in self.sub_catalog_links[0:15]:
-                async with session.get(url=link, headers=self.headers) as response:
-                    try:
-                        html = await response.text()
-                        soup = BeautifulSoup(html, "lxml")
-                        pagination = soup.find("div", class_ = "pagination").find_all("li")
-                        if pagination==None:
-                            print(f'link IF{link}')
-                        else:
-                            # print(f'PAGINATION ELSE{pagination}')
-                            # print(f'LAST:{pagination[-2]}')
-                            page_first = pagination[-2].find('a')
-                            # print(f'PAGE_FIRST{page_first}')
-                            last_page = page_first.text
-                            # print(last_page)
-                            page_first_href = page_first['href']
-                            link_page_first = page_first_href.split('/')[1]
-                            page_first_url = f'{self.basic_url}/{link_page_first}.html'
-                            self.all_links_category.append(page_first_url)
-                            for i in range(2, int(last_page)+1):
-                                url_with_pagination =f'{self.basic_url}/{link_page_first}/page-{i}.html'
-                                print(url_with_pagination)
-                                self.all_links_category.append(url_with_pagination)
-
-
-                    except:
-                        # print(f'EXEPT LINK{link}')
-                        self.all_links_category.append(link)
-
-
-
+    async def get_pagination(self, session, link, num):
+            async with session.get(url=link, headers=self.headers) as response:
+                try:
+                    html = await response.text()
+                    soup = BeautifulSoup(html, "lxml")
+                    pagination = soup.find("div", class_ = "pagination").find_all("li")
+                    page_first = pagination[-2].find('a')
+                    print(f'PAGE_FIRST{page_first}')
+                    last_page = page_first.text
+                    print(last_page)
+                    page_first_href = page_first['href']
+                    link_page_first = page_first_href.split('/')[1]
+                    page_first_url = f'{self.basic_url}/{link_page_first}.html'
+                    self.all_links_category.append(page_first_url)
+                    for i in range(2, int(last_page)+1):
+                        url_with_pagination = f'{self.basic_url}/{link_page_first}/page-{i}.html'
+                        print(url_with_pagination)
+                        self.all_links_category.append(url_with_pagination)
+                except:
+                    print(f'EXEPT LINK{link}')
+                    self.all_links_category.append(link)
 
     async def gather_all_catalog_tasks(self):
         async with aiohttp.ClientSession() as session:
@@ -81,13 +69,60 @@ class Parser:
                 all_catalog_tasks.append(task)
             await asyncio.gather(*all_catalog_tasks)
 
+    async def gather_pagination_tasks(self):
+        async with aiohttp.ClientSession() as session:
+            pagination_task = []
+            for num, link in enumerate(self.sub_catalog_links[0:15]):
+                task = asyncio.create_task(self.get_pagination(session, link, num))
+                pagination_task.append(task)
+            await asyncio.gather(*pagination_task)
+
+    async def get_pages_products(self, session, link, num):
+                async with session.get(url = link, headers = self.headers) as response:
+                    html = await response.text()
+                    soup = BeautifulSoup(html, 'lxml')
+                    products_link = soup.find_all('div', class_ = 'products-list_item-wrap')
+                    for i in products_link:
+                        link = i.find('a')
+                        href = link['href']
+                        url = f'{self.basic_url}{href}'
+                        print(url)
+                        self.all_products_links.append(url)
+
+    async def gather_pages_tasks(self):
+        async with aiohttp.ClientSession() as session:
+            pages_tasks = []
+            for num, link in enumerate(self.all_links_category[0:15]):
+                task = asyncio.create_task(self.get_pages_products(session, link, num))
+                pages_tasks.append(task)
+            await asyncio.gather(*pages_tasks)
+
+    async def load_products(self, session, link, num):
+                async with session.get(url = link, headers = self.headers) as response:
+                    html = await response.text()
+                    soup = BeautifulSoup(html, 'lxml')
+                    try:
+                        h1 = soup.h1.string.strip()
+                        print(h1)
+                    except:
+                        pass
+
+    async def gather_load_products_tasks(self):
+        async with aiohttp.ClientSession() as session:
+            load_pages_tasks = []
+            for num, link in enumerate(self.all_products_links[0:40]):
+                task = asyncio.create_task(self.load_products(session, link, num))
+                load_pages_tasks.append(task)
+            await asyncio.gather(*load_pages_tasks)
+
 
     def main(self):
         asyncio.get_event_loop().run_until_complete(self.get_main_catalog_links())
         asyncio.get_event_loop().run_until_complete(self.gather_all_catalog_tasks())
-        print(self.sub_catalog_links)
-        asyncio.get_event_loop().run_until_complete(self.get_pagination())
-        print(self.all_links_category)
+        asyncio.get_event_loop().run_until_complete(self.gather_pagination_tasks())
+        asyncio.get_event_loop().run_until_complete(self.gather_pages_tasks())
+        asyncio.get_event_loop().run_until_complete(self.gather_load_products_tasks())
+        print(self.all_products_links)
 
 if __name__ == "__main__":
     parser = Parser()
